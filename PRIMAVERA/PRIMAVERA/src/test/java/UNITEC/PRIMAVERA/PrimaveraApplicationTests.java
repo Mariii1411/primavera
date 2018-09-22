0a34a8cf9bddeef24261d3e63bbfb74517ba3c9b
T-107 <T-107@PC185>
2018-09-21 21:35:28 -0500

    primavera 21/09/18

---
+++ PRIMAVERA/PRIMAVERA/src/test/java/UNITEC/PRIMAVERA/PrimaveraApplicationTests.java
@@ -0,0 +1,16 @@
+package UNITEC.PRIMAVERA;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrimaveraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
