6530de847ea464c85fafecd9f7c49315e683bdbc
T-107 <T-107@PC185>
2018-09-25 21:44:50 -0500

    inyeccion de dependencia 25/09/18

--- PRIMAVERA/src/main/java/UNITEC/PRIMAVERA/PrimaveraApplication.java
+++ PRIMAVERA/src/main/java/UNITEC/PRIMAVERA/PrimaveraApplication.java
@@ -1,12 +1,23 @@
 package UNITEC.PRIMAVERA;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrimaveraApplication {
+public class PrimaveraApplication implements CommandLineRunner{   //corre el main con ejecuciones 
+    int x;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {  //
 		SpringApplication.run(PrimaveraApplication.class, args);
+                
+            
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+      //To change body of generated methods, choose Tools | Templates.
+    x=2;
+        System.out.println("Hola mundo");
+    }
 }
